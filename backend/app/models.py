b43dfdd9c5af96fa48c75ca8e99ad72aa125836d
rlemiere <rlemiere@athoria.fr>
2026-06-25 15:03:16 +0200

    feat(backend): adds created_at field on model

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import String
+from datetime import datetime
+
+from sqlalchemy import DateTime, String, func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
@@ -11,3 +13,8 @@ class Link(Base):
 
     id: Mapped[str] = mapped_column(String(8), primary_key=True)
     url: Mapped[str] = mapped_column(String, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        index=True,
+    )
